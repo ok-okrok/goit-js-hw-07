4be6240eb897691461c0171f9ccc296ead5fe023
Orest Krokhmalnyi <kr.orest@gmail.com>
2021-01-26 21:51:43 +0200

    hw07-04

--- js/task5.js
+++ js/task5.js
@@ -8,9 +8,10 @@ let input = document.getElementById("name-input");
 let nameOutput = document.getElementById("name-output");
 
 input.oninput = function () {
-    if (input.value === '') {
-        nameOutput.innerHTML = 'незнакомец';
-    } else {
-        nameOutput.innerHTML = input.value;
-    }
+    // if (input.value === '') {
+    //     nameOutput.innerHTML = 'незнакомец';
+    // } else {
+    //     nameOutput.innerHTML = input.value;
+    // }
+    nameOutput.innerHTML = input.value || 'незнакомец';
 };
--- js/task6.js
+++ js/task6.js
@@ -28,16 +28,15 @@ let totalLenght = inputValue.getAttribute("data-length");
 let intTotallenght = parseInt(totalLenght, 10);
 
 inputValue.oninput = function () {
-    // if (inputValue.value.length === intTotallenght) {
-    //     inputValue.classList.remove("invalid");
-    //     inputValue.classList.add("valid");
-    // }
-    // if (inputValue.value.length === 0) {
-    //     inputValue.classList.remove("valid");
-    //     inputValue.classList.remove("invalid");
-    // }
-    // if (inputValue.value.length !== intTotallenght && inputValue.value.length !== 0) {
-    //     inputValue.classList.add("invalid");
-    // }
-    nameOutput.innerHTML = input.value || 'незнакомец';
+    if (inputValue.value.length === intTotallenght) {
+        inputValue.classList.remove("invalid");
+        inputValue.classList.add("valid");
+    }
+    if (inputValue.value.length === 0) {
+        inputValue.classList.remove("valid");
+        inputValue.classList.remove("invalid");
+    }
+    if (inputValue.value.length !== intTotallenght && inputValue.value.length !== 0) {
+        inputValue.classList.add("invalid");
+    }
 };
